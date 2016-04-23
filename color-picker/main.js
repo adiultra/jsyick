32e3e89cb96611a635e0691eeafea47af241f50e
Aditya Ultra <adityaultra4@gmail.com>
2016-04-23 13:54:49 +0530

    Improved look on the color picker :smirk:

--- color-picker/main.js
+++ color-picker/main.js
@@ -1,27 +1,5 @@
 'use strict';
 
-function setBgColorById(id, sColor) {
-    var elem;
-
-    if (document.getElementById) {
-        if (elem = document.getElementById(id)) {
-            if (elem.style) {
-                elem.style.backgroundColor = sColor;
-
-                return 1;   // success
-            }
-        }
-    }
-
-    return 0;               // failure
-}
-
-setBgColorById('color-1', '#fea');
-setBgColorById('color-2', '#7f7');
-setBgColorById('color-3', '#fe7');
-setBgColorById('color-4', '#5fa');
-setBgColorById('color-5', '#f53');
-
 function update1(jscolor) {
     // 'jscolor' instance can be used as a string
     document.getElementById('color-1').style.backgroundColor = '#' + jscolor
@@ -43,9 +21,3 @@ function update5(jscolor) {
     // 'jscolor' instance can be used as a string
     document.getElementById('color-5').style.backgroundColor = '#' + jscolor
 }
-
-update1('#fea');
-update2('#7f7');
-update3('#fe7');
-update4('#5fa');
-update5('#f53');
